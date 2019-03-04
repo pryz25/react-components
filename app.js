92a8a11b3938772d55d53d47c04fc8a277989a21
pryz25 <pryzanych@gmail.com>
2019-03-04 15:38:19 -0800

    Completed Intro

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> 
+  </div>
+);
+
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+    
+    return(
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+    );
+  }
+}
+
+var TodoList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <TodoListItem todo={todo} />
+    )}
+  </ul>
+);
+
+ReactDOM.render(<App />, 
+  document.getElementById("app")
+);
